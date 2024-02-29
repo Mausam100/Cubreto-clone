87ed00ab3ecdef36fbc536144b4a5a6845dbf92e
Mausam100 <smausam104@gmail.com>
2024-02-29 13:11:39 +0530

    second

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ Shery.makeMagnet(".zoom");
 Shery.hoverWithMediaCircle(".hv", {
   videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
 });
+
 gsap.to(".fleftelem", {
   scrollTrigger:{
       trigger:"#fimages",
@@ -15,22 +16,24 @@ gsap.to(".fleftelem", {
   y: "-300%",
   ease: Power1,
 
-})
-let sections = document.querySelectorAll(".fleftelem");
-Shery.imageEffect(".images", {
-  style: 5,
-  slideStyle: (setScroll) => {
-   sections.forEach(function(section, index) {
-    ScrollTrigger.create({
-      trigger: section,
-      start: "top top",
-      scrub: 1,
-      onUpdate: function(prog){
-        setScroll(prog.progress+index)
-      },
-    });
-    
-   });
-  },
 });
+
+
+// let sections = document.querySelectorAll(".fright");
+// Shery.imageEffect(".images", {
+//   style: 5,
+//   slideStyle: (setScroll) => {
+//    sections.forEach(function(section,       index) {
+//     ScrollTrigger.create({
+//       trigger: section,
+//       start: "top top",
+//       scrub: 1,
+//       onUpdate: function(prog){
+//         setScroll(prog.progress+index)
+//       },
+//     });
+    
+//    });
+//   },
+// });
 
